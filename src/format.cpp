1b78396e39c545abb2d879b72464c63aca965d82
Jono Tassia <jonotassia@gmail.com>
2023-02-27 21:44:48 +1100

    Updated time functions

--- src/format.cpp
+++ src/format.cpp
@@ -8,12 +8,8 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 string Format::ElapsedTime(long seconds) { 
-    // Get current time then subtract seconds to get delta
-    long current_time = std::time(0);
-    long delta = current_time - seconds;
-    
     // Divide until you get hours, minutes, and seconds
-    auto result = div(delta, (long)3600);
+    auto result = div(seconds, (long)3600);
     string hours_elapsed = std::to_string(result.quot);
 
     result = div(result.rem, (long)60);
