b74fb0895df7a6c38a7e060be2c346c25268914e
Jono Tassia <jonotassia@gmail.com>
2023-02-27 15:11:17 +1100

    Bug fixes

--- include/process.h
+++ include/process.h
@@ -8,7 +8,7 @@ It contains relevant attributes as shown below
 */
 class Process {
  public:
-  Processor(int pid);
+  Process(int pid);
   int Pid() const;                               // TODO: See src/process.cpp
   std::string User();                      // TODO: See src/process.cpp
   std::string Command();                   // TODO: See src/process.cpp
